d37131e60f71f894d643767817fd60184beb20aa
mutassem <mutassemalmahamid@gmail.com>
2025-10-01 08:19:59 +0300

    update 2

--- src/main/java/com/example/ecommerce/config/WebConfig.java
+++ src/main/java/com/example/ecommerce/config/WebConfig.java
@@ -7,18 +7,18 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/api/**")
-                .allowedOrigins(
-                        "http://localhost:3000",
-                        "http://127.0.0.1:3000",
-                        "http://localhost:3001",
-                        "https://zealous-bush-0e2516d03.2.azurestaticapps.net"
-                )
-                .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
-                .allowedHeaders("*")
-                .allowCredentials(true)
-                .maxAge(3600);
-    }
+//    @Override
+//    public void addCorsMappings(CorsRegistry registry) {
+//        registry.addMapping("/api/**")
+//                .allowedOrigins(
+//                        "http://localhost:3000",
+//                        "http://127.0.0.1:3000",
+//                        "http://localhost:3001",
+//                        "https://zealous-bush-0e2516d03.2.azurestaticapps.net"
+//                )
+//                .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
+//                .allowedHeaders("*")
+//                .allowCredentials(true)
+//                .maxAge(3600);
+//    }
 }
